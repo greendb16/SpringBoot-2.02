abb28fd81e84e64c5442b7832ddcc5df2fcc7034
Greendb16 <greendb16@gmail.com>
2019-05-20 16:43:37 -0400

    First Exercise Push

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class Song {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private int rating;
+    private int year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
