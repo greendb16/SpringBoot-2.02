f726ad8da6c66ed4fafdd942500bace9b31cb476
Greendb16 <greendb16@gmail.com>
2019-05-20 17:01:45 -0400

    Fixed getters and setters

--- src/main/java/com/example/demo/Song.java
+++ src/main/java/com/example/demo/Song.java
@@ -15,4 +15,44 @@ public class Song {
     public void setYear(int year) {
         this.year = year;
     }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
 }
